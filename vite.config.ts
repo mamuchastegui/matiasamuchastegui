ce903006185588815de70c3c5d6b654f3a97c193
AxSRockS <axsrocks@gmail.com>
2025-04-02 13:45:39 -0300

    vite config tunnel

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,11 @@ export default defineConfig({
       },
     },
   },
+  server: {
+    ...(process.env.NODE_ENV === 'development' && {
+      allowedHosts: true,
+    }) 
+  },
   plugins: [react()],
   resolve: {
     alias: {

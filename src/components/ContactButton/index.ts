9d4f75ef4cbf645f6ed84e3e1c16da4814eaad3a
AxSRockS <axsrocks@gmail.com>
2025-05-03 17:42:19 -0300

    build

--- src/components/ContactButton/index.ts
+++ src/components/ContactButton/index.ts
@@ -1,5 +1,3 @@
-import ContactButton from './ContactButton';
-
 export { default } from './ContactButton';
 export { default as ShinyText } from './ShinyText';
 export { default as StarBorder } from './StarBorder'; 

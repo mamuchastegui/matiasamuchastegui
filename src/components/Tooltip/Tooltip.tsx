1be40c1a90516d78923687bece969967cc6c39eb
AxSRockS <axsrocks@gmail.com>
2025-07-04 23:02:37 -0300

    build fix

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled, { css } from 'styled-components';
 import { useTheme } from '../../context/ThemeContext';
 
